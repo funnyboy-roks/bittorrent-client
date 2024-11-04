2005d6803e42b1763273988eaaa6f3d7bf261516
funnyboy-roks <funnyboyroks@gmail.com>
2024-11-03 19:18:48 -0600

    add peers command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use anyhow::{bail, Context};
+use ::reqwest::Url;
+use anyhow::Context;
+use core::str;
 use nom::{
     branch::alt,
     bytes::streaming::take,
@@ -7,12 +9,87 @@ use nom::{
     sequence::{delimited, terminated, tuple},
     IResult,
 };
-use serde::Serialize;
-use std::{collections::HashMap, env, fmt::Display, fs::File, ops::Index};
+use reqwest::blocking as reqwest;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use sha1::{Digest, Sha1};
+use std::{
+    collections::HashMap,
+    env,
+    fmt::Display,
+    io::Read,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    ops::Index,
+    str::FromStr,
+};
 
+#[derive(Debug, Clone, Deserialize)]
+pub struct PeersResponse {
+    pub interval: usize,
+    pub peers: Vec<u8>,
+}
+
+impl PeersResponse {
+    pub fn peers(&self) -> impl Iterator<Item = SocketAddr> + use<'_> {
+        self.peers.chunks_exact(6).map(|chunk| {
+            let (ip, port) = chunk.split_at(4);
+            let [a, b, c, d] = ip.try_into().unwrap();
+            SocketAddr::new(
+                IpAddr::V4(Ipv4Addr::new(a, b, c, d)),
+                u16::from_be_bytes(port.try_into().unwrap()),
+            )
+        })
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct TorrentInfo {
+    pub length: usize,
+    pub name: String,
+    #[serde(rename = "piece length")]
+    pub piece_length: usize,
+    pub pieces: Vec<u8>,
+}
+
+impl TorrentInfo {
+    fn pieces(&self) -> impl Iterator<Item = &[u8]> {
+        self.pieces.chunks_exact(20)
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Torrent {
+    pub announce: String,
+    pub info: TorrentInfo,
+}
+
+pub fn serde<S, D>(s: &S) -> anyhow::Result<D>
+where
+    S: Serialize,
+    D: DeserializeOwned,
+{
+    Ok(
+        serde_json::from_str(&serde_json::to_string(s).context("serializing value")?)
+            .context("deserializing value")?,
+    )
+}
+
+#[derive(Debug, Clone)]
+pub struct Decoded<'a> {
+    pub source: &'a [u8],
+    pub kind: DecodedKind<'a>,
+}
+
+impl Serialize for Decoded<'_> {
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.kind.serialize(s)
+    }
+}
 #[derive(Debug, Clone, Serialize)]
 #[serde(untagged)]
-pub enum Decoded<'a> {
+pub enum DecodedKind<'a> {
     Bytes(&'a [u8]),
     String(&'a str),
     Int(i64),
@@ -20,12 +97,18 @@ pub enum Decoded<'a> {
     Dict(HashMap<&'a str, Decoded<'a>>),
 }
 
+impl<'a> DecodedKind<'a> {
+    pub fn into_decoded(self, source: &'a [u8]) -> Decoded<'a> {
+        Decoded { source, kind: self }
+    }
+}
+
 impl<'a> Index<&'_ str> for Decoded<'a> {
     type Output = Decoded<'a>;
 
     fn index(&self, index: &'_ str) -> &Self::Output {
-        match self {
-            Decoded::Dict(d) => &d[index],
+        match &self.kind {
+            DecodedKind::Dict(d) => &d[index],
             _ => panic!("Cannot index with string into type other than dictionary"),
         }
     }
@@ -35,8 +118,8 @@ impl<'a> Index<usize> for Decoded<'a> {
     type Output = Decoded<'a>;
 
     fn index(&self, index: usize) -> &Self::Output {
-        match self {
-            Decoded::List(d) => &d[index],
+        match &self.kind {
+            DecodedKind::List(d) => &d[index],
             _ => panic!("Cannot index with usize into type other than list"),
         }
     }
@@ -44,17 +127,17 @@ impl<'a> Index<usize> for Decoded<'a> {
 
 impl Display for Decoded<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Decoded::Bytes(b) => {
+        match &self.kind {
+            DecodedKind::Bytes(b) => {
                 write!(f, "0x")?;
                 for b in b.iter() {
                     write!(f, "{:02x}", b)?;
                 }
                 Ok(())
             }
-            Decoded::String(s) => write!(f, "{}", s),
-            Decoded::Int(n) => write!(f, "{}", n),
-            Decoded::List(l) => {
+            DecodedKind::String(s) => write!(f, "{}", s),
+            DecodedKind::Int(n) => write!(f, "{}", n),
+            DecodedKind::List(l) => {
                 write!(f, "[")?;
                 for (i, d) in l.iter().enumerate() {
                     if i > 0 {
@@ -64,7 +147,7 @@ impl Display for Decoded<'_> {
                 }
                 write!(f, "]")
             }
-            Decoded::Dict(l) => {
+            DecodedKind::Dict(l) => {
                 write!(f, "{{")?;
                 for (i, (key, value)) in l.iter().enumerate() {
                     if i > 0 {
@@ -78,93 +161,38 @@ impl Display for Decoded<'_> {
     }
 }
 
-fn _decode_bencoded_value(encoded: &[u8]) -> anyhow::Result<(Decoded<'_>, &[u8])> {
-    match encoded {
-        [b'd', rest @ ..] => {
-            let mut items = HashMap::new();
-            let mut rest = rest;
-            while rest[0] != b'e' {
-                let (key_decoded, new_rest) = _decode_bencoded_value(rest)?;
-                let (value_decoded, new_rest) = _decode_bencoded_value(new_rest)?;
-                rest = new_rest;
-                match key_decoded {
-                    Decoded::String(key) => {
-                        items.insert(key.into(), value_decoded);
-                    }
-                    _ => bail!("Expected key to be of type string, found {:?}", key_decoded),
-                }
-            }
-            let rest = rest.strip_prefix(b"e").unwrap();
-            Ok((Decoded::Dict(items), rest))
-        }
-        [b'l', rest @ ..] => {
-            let mut items = Vec::new();
-            let mut rest = rest;
-            while rest[0] != b'e' {
-                let (decoded, new_rest) = _decode_bencoded_value(rest)?;
-                rest = new_rest;
-                items.push(decoded);
-            }
-            let rest = rest.strip_prefix(b"e").unwrap();
-            Ok((Decoded::List(items), rest))
-        }
-        [b'i', rest @ ..] => {
-            let e = rest
-                .iter()
-                .position(|b| *b == b'e')
-                .context("looking for end of integer")?;
-            let (num, rest) = rest.split_at(e);
-            let rest = &rest[1..];
-            let num = String::from_utf8(num.to_vec())?;
-            dbg!(&num);
-            let num: i64 = num.parse()?;
-            Ok((Decoded::Int(num), rest))
-        }
-        [b'0'..=b'9', ..] => {
-            let split = encoded
-                .iter()
-                .position(|b| *b == b':')
-                .context("need colon for string")?;
-            let (len, rest) = encoded.split_at(split);
-            let rest = &rest[1..];
-            let len: usize = String::from_utf8(len.to_vec())?.parse()?;
-            let (s, rest) = rest.split_at(len);
-            if let Ok(s) = std::str::from_utf8(s) {
-                Ok((Decoded::String(s), rest))
-            } else {
-                Ok((Decoded::Bytes(s), rest))
-            }
-        }
-        _ => panic!(
-            "Unhandled encoded value: {}",
-            String::from_utf8(encoded.to_vec()).unwrap_or_else(|_| format!("{:?}", encoded))
-        ),
-    }
-}
-
 fn string(encoded: &[u8]) -> IResult<&[u8], Decoded<'_>> {
     let (rest, len) = terminated(u64, char(':'))(encoded)?;
     let (rest, s) = take(len)(rest)?;
+    let source = encoded
+        .strip_suffix(rest)
+        .expect("rest is the end of `encoded`");
     if let Ok(string) = std::str::from_utf8(s) {
-        Ok((rest, Decoded::String(string)))
+        Ok((rest, DecodedKind::String(string).into_decoded(source)))
     } else {
-        Ok((rest, Decoded::Bytes(s)))
+        Ok((rest, DecodedKind::Bytes(s).into_decoded(source)))
     }
 }
 
 fn int(encoded: &[u8]) -> IResult<&[u8], Decoded> {
     let (rest, n) = delimited(char('i'), i64, char('e'))(encoded)?;
-    Ok((rest, Decoded::Int(n)))
+    let slice = encoded
+        .strip_suffix(rest)
+        .expect("rest is the end of `encoded`");
+    Ok((rest, DecodedKind::Int(n).into_decoded(slice)))
 }
 
 fn list(encoded: &[u8]) -> IResult<&[u8], Decoded<'_>> {
     let (rest, vec) = delimited(char('l'), many0(decode_bencoded_value), char('e'))(encoded)?;
-    Ok((rest, Decoded::List(vec)))
+    let slice = encoded
+        .strip_suffix(rest)
+        .expect("rest is the end of `encoded`");
+    Ok((rest, DecodedKind::List(vec).into_decoded(slice)))
 }
 
 fn dict_entry(encoded: &[u8]) -> IResult<&[u8], (&str, Decoded<'_>)> {
     let (rest, (key, value)) = tuple((string, decode_bencoded_value))(encoded)?;
-    let Decoded::String(key) = key else {
+    let DecodedKind::String(key) = key.kind else {
         panic!("should always be string");
     };
     Ok((rest, (key, value)))
@@ -172,7 +200,13 @@ fn dict_entry(encoded: &[u8]) -> IResult<&[u8], (&str, Decoded<'_>)> {
 
 fn dict(encoded: &[u8]) -> IResult<&[u8], Decoded<'_>> {
     let (rest, vec) = delimited(char('d'), many0(dict_entry), char('e'))(encoded)?;
-    Ok((rest, Decoded::Dict(vec.into_iter().collect())))
+    let slice = encoded
+        .strip_suffix(rest)
+        .expect("rest is the end of `encoded`");
+    Ok((
+        rest,
+        DecodedKind::Dict(vec.into_iter().collect()).into_decoded(slice),
+    ))
 }
 
 fn decode_bencoded_value(encoded: &[u8]) -> IResult<&[u8], Decoded<'_>> {
@@ -191,14 +225,63 @@ fn main() -> anyhow::Result<()> {
         "info" => {
             let file = std::fs::read(&args[2])?;
             let (_, value) = decode_bencoded_value(&file).unwrap();
-            // dbg!(value);
-            let Decoded::Dict(dict) = value else {
-                bail!("expected dict, got {:?}", value);
-            };
-            println!("Tracker URL: {}", dict["announce"]);
-            println!("Length: {}", dict["info"]["length"]);
+
+            let data: Torrent = serde(&value)?;
+
+            println!("Tracker URL: {}", data.announce);
+            println!("Length: {}", data.info.length);
+            let mut hasher = Sha1::new();
+            hasher.update(value["info"].source);
+            let info_hash = hasher.finalize();
+            println!("Info Hash: {}", hex::encode(info_hash));
+            println!("Piece Length: {}", data.info.piece_length);
+            println!("Piece Hashes:");
+            for piece in data.info.pieces() {
+                println!("{}", hex::encode(piece));
+            }
         }
-        _ => {}
+        "peers" => {
+            let file = std::fs::read(&args[2])?;
+            let (_, value) = decode_bencoded_value(&file).unwrap();
+
+            let data: Torrent = serde(&value)?;
+
+            eprintln!("Tracker URL: {}", data.announce);
+            eprintln!("Length: {}", data.info.length);
+            let mut hasher = Sha1::new();
+            hasher.update(value["info"].source);
+            let info_hash = hasher.finalize();
+            eprintln!("Info Hash: {}", hex::encode(info_hash));
+            eprintln!("Piece Length: {}", data.info.piece_length);
+            eprintln!("Piece Hashes:");
+            for piece in data.info.pieces() {
+                eprintln!("{}", hex::encode(piece));
+            }
+
+            let mut url = Url::from_str(&data.announce)?;
+            url.query_pairs_mut()
+                .append_pair("info_hash", unsafe { str::from_utf8_unchecked(&info_hash) })
+                .append_pair("peer_id", "20 chars is too shor")
+                .append_pair("port", "6881")
+                .append_pair("uploaded", "0")
+                .append_pair("downloaded", "0")
+                .append_pair("left", &data.info.length.to_string())
+                .append_pair("compact", "1");
+            dbg!(url.to_string());
+            let res = reqwest::get(url)?;
+            let text = res.bytes()?;
+            dbg!(&text);
+            let (_, res) = decode_bencoded_value(&text).unwrap();
+            let res: PeersResponse = serde(&res)?;
+            dbg!(&res);
+
+            for peer in res.peers() {
+                println!("{}", peer);
+            }
+        }
+        command => {
+            panic!("Unknown command '{}'", command)
+        }
     }
     Ok(())
 }
